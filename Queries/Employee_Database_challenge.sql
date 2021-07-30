1e0f28d14fcdc12c5798f954b450923848bdd57a
aseo67 <85456812+aseo67@users.noreply.github.com>
2021-07-29 22:52:34 -0700

    Update Challenge

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -1,1 +1,55 @@
 -- Deliverable 1: Number of Retiring Employees by Title
+SELECT e.emp_no, 
+	e.first_name, 
+	e.last_name, 
+	t.title,
+	t.from_date,
+	t.to_date
+INTO retirement_titles
+FROM employees AS e
+JOIN titles AS t
+ON (e.emp_no = t.emp_no)
+WHERE (e.birth_date >= '1952-01-01' AND e.birth_date <= '1955-12-31')
+ORDER BY e.emp_no ASC;
+
+SELECT * FROM retirement_titles;
+
+-- Use Distinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (emp_no) emp_no,
+	first_name,
+	last_name,
+	title
+INTO unique_titles
+FROM retirement_titles
+ORDER BY emp_no ASC, to_date DESC;
+
+SELECT * FROM unique_titles;
+
+-- Number of titles from Unique Titles table
+SELECT COUNT(title) AS "count", title
+INTO retiring_titles
+FROM unique_titles
+GROUP BY title
+ORDER BY "count" DESC;
+
+SELECT * FROM retiring_titles;
+
+-- Deliverable 2: Employees Eligible for Mentorship Program
+SELECT DISTINCT ON (e.emp_no) e.emp_no, 
+	e.first_name, 
+	e.last_name,
+	e.birth_date,
+	de.from_date,
+	de.to_date,
+	t.title
+INTO mentorship_eligibility
+FROM employees AS e
+JOIN dept_emp AS de
+ON (e.emp_no = de.emp_no)
+JOIN titles AS t
+ON (e.emp_no = t.emp_no)
+WHERE (de.to_date = '9999-01-01') 
+AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY e.emp_no ASC;
+
+SELECT * FROM mentorship_eligibility;
